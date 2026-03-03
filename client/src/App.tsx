97e0d248175cce1abac6c18902af91c16c53c0f0
rhvetican <ranitz.2005@gmail.com>
2026-03-03 14:38:49 +0530

    refactor(routes): update routing for history and settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Upload from "./pages/Upload";
+import History from "./pages/History";
 import Settings from "./pages/Settings";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
         <Routes>
             <Route element={<Layout />}>
                 <Route path="/" element={<Upload />} />
+                <Route path="/history" element={<History />} />
                 <Route path="/settings" element={<Settings />} />
             </Route>
         </Routes>
